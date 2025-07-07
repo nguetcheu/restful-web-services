9561854131546d15e6b993fd9db5ad874ef13676
nguetcheu <dnguetcheu@gmail.com>
2025-07-07 10:53:21 +0200

    Add path variable support to helloWorldBean method

--- src/main/java/com/learning/nkd/restful_web_services/Controller/HelloWorldController.java
+++ src/main/java/com/learning/nkd/restful_web_services/Controller/HelloWorldController.java
@@ -2,7 +2,7 @@ package com.learning.nkd.restful_web_services.Controller;
 
 import com.learning.nkd.restful_web_services.Entity.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +16,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
